d8d5bcf20c42d65696e7c2e483e7ff73ca81e3aa
Jose Antonio Sanchez <jose.sanchezm@atos.net>
2019-05-27 12:27:15 +0200

    Add repository documentation

--- persistence/model.go
+++ persistence/model.go
@@ -20,11 +20,26 @@ type AlreadyExistsError struct {
 	ID          string
 }
 
+// PaymentRepository is the interface that any persistence backend must implement.
+// It contains the basic CRUD operations for individual payments
+// (AddPayment, GetPayment, UpdatePayment and DeletePayment)
+// plus an operation that must return a list of payments filtered by arbitrary parameters.
 type PaymentRepository interface {
+	// AddPayment must save the payment information passed as parameter in the persistence backend asigning it a unique identifier.
+	// It must return the saved document with this new identifier or an error if something unexpected happens
 	AddPayment(pay payment.Payment) (payment.Payment, error)
+	// UpdatePayment must replace the payment information whose identifier is in the input object with the information present in the input parameter.
+	// It must return the updated payment information or an error if something goes wrong or the payment with such identifier does not exist.
 	UpdatePayment(pay payment.Payment) (payment.Payment, error)
+	// DeletePayment must delete the payment information whose identifier matches with the one passed as parameter.
+	// It must return the deleted object or an error if something goes wrong or the payment with such identifier does not exist.
 	DeletePayment(id string) (payment.Payment, error)
+	// GetPayment must return the payment information whose identifier matches with the one passed as parameter.
+	// It must return the payment information or an error if something goes wrong or the payment with such identifier does not exist.
 	GetPayment(id string) (payment.Payment, error)
+	// GetPayments must return a list of payments which match the filters passed as parameter.
+	// If this parameter is nil or empty, it must return the whole list of payments available in the persistence backend.
+	// In case of error, it must be returned as second parameter.
 	GetPayments(filter map[string]string) ([]payment.Payment, error)
 }
 
